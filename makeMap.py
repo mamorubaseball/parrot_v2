0bb0a093a0c1b07bce78d57467eef4b6066a9c96
mamoru9045 <mamoru@sandbox-vr.com>
2022-12-14 15:47:46 +0900

    3D地図作成プログラム修正

--- makeMap.py
+++ makeMap.py
@@ -10,12 +10,9 @@ from olympe.messages.ardrone3.PilotingState import FlyingStateChanged
 from photo import take_photo
 from olympe.messages import gimbal
 import math
+import argparse
 
-# 4つの変数
-# GPS1:35.615429, 139.299958
-# GPS2:35.61503573425537, 139.299653451925
-# h = 40m
-# L = 5m
+# 変数の指定
 DRONE_IP = os.environ.get("DRONE_IP", "192.168.42.1")
 H = 40
 L = 5
@@ -35,29 +32,37 @@ def set_gimbal(drone,angle):
 )).wait()
 
 def main():
+    parser = argparse.ArgumentParser(description='飛行パラメータの指定をここで行う')
+    parser.add_argument('-H','--height',default=20)
+    parser.add_argument('-L','--height',default=2)
     drone = olympe.Drone("192.168.42.1")
     drone.connect()
+
     angle = math.pi/4
     set_gimbal(drone=drone,angle=angle)
     time.sleep(2)
     assert drone(TakeOff()).wait().success()
     # 飛行プログラム開始
     drone(moveBy(0,0, -H, 0)).wait().success()
-    drone(extended_move_to(35.615429, 139.299958,40)).wait().success()
+    # drone(extended_move_to(35.615429, 139.299958,40)).wait().success()
     
     for i in range(13):
-        for j in range(10):
-            if i%2 == 0:
-                if i%4 == 0:l = L
-                else:l = -L
-                drone(moveBy(0,l,0, 0)).wait().success()
-                take_photo(drone)
-                time.sleep(2)
-            #iが奇数の時は5m前に進む
-            elif i%2 == 1:
-                drone(moveBy(L,0,0,0)).wait().success()
-                time.sleep(2)
+        # iが偶数の時は左右に動く
+        if i%2 == 0:
+            for j in range(10):
+                if i%4 == 0:
+                    drone(moveBy(0,L,0, 0)).wait().success()
+                    take_photo(drone)
+                    time.sleep(2)
+                else:
+                    drone(moveBy(0,-L,0, 0)).wait().success()
+                    take_photo(drone)
+                    time.sleep(2)
+        else:
+            drone(moveBy(L,0,0,0)).wait().success()
+            time.sleep(2)
     assert drone(Landing()).wait().success()
+
 
 if __name__ == "__main__":
     main()
